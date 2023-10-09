ba29de4cdb9f632520b841e451762a85235d1903
esceer <roland.kts@gmail.com>
2023-10-09 02:15:05 +0200

    Add neural network visualizer

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 500;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 500;
+const carCtx = carCanvas.getContext("2d");
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9, 5);
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 700;
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 5);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "black", Driver.AI);
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "green", Driver.NPC, 2),
@@ -27,8 +31,10 @@ const traffic = [
 
 animate();
 
-function animate() {
-    canvas.height = window.innerHeight;
+function animate(time) {
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
+
     for (const npc of traffic) {
         npc.update(road.borders, []);
     }
@@ -36,15 +42,18 @@ function animate() {
 
 
     // to move the road instead of the car
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
 
-    road.draw(ctx);
+    road.draw(carCtx);
     for (const npc of traffic) {
-        npc.draw(ctx);
+        npc.draw(carCtx);
     }
-    car.draw(ctx);
+    car.draw(carCtx);
 
-    ctx.restore();
+    carCtx.restore();
+
+    networkCtx.lineDashOffset = -time / 50;
+    Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }
